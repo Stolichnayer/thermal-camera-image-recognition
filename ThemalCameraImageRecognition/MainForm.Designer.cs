ec3d2e768776890af3eaf19eca8affd78e2d1967
Alex Perrakis <alexperrakis1@hotmail.com>
2021-02-23 21:51:27 +0200

    - Finished form design - Added Color RGB Code per Pixel function

---
+++ ThemalCameraImageRecognition/MainForm.Designer.cs
@@ -0,0 +1,283 @@
+﻿
+namespace ThemalCameraImageRecognition
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.panelImage = new System.Windows.Forms.Panel();
+            this.btnConvertToGray = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.labelPixelsDesc = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panelColor = new System.Windows.Forms.Panel();
+            this.labelColor = new System.Windows.Forms.Label();
+            this.labelPixels = new System.Windows.Forms.Label();
+            this.labelColorDesc = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelIntensityDesc = new System.Windows.Forms.Label();
+            this.panelImage.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(333, 20);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(98, 22);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(73, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(254, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Image";
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Enabled = false;
+            this.btnProcess.Location = new System.Drawing.Point(437, 20);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(98, 22);
+            this.btnProcess.TabIndex = 3;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            // 
+            // panelImage
+            // 
+            this.panelImage.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panelImage.Controls.Add(this.btnConvertToGray);
+            this.panelImage.Controls.Add(this.pictureBox);
+            this.panelImage.Location = new System.Drawing.Point(0, 59);
+            this.panelImage.Name = "panelImage";
+            this.panelImage.Size = new System.Drawing.Size(798, 614);
+            this.panelImage.TabIndex = 4;
+            // 
+            // btnConvertToGray
+            // 
+            this.btnConvertToGray.Enabled = false;
+            this.btnConvertToGray.Location = new System.Drawing.Point(638, 563);
+            this.btnConvertToGray.Name = "btnConvertToGray";
+            this.btnConvertToGray.Size = new System.Drawing.Size(112, 22);
+            this.btnConvertToGray.TabIndex = 4;
+            this.btnConvertToGray.Text = "Convert to grayscale";
+            this.btnConvertToGray.UseVisualStyleBackColor = true;
+            this.btnConvertToGray.Click += new System.EventHandler(this.btnConvertToGray_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox.Location = new System.Drawing.Point(50, 57);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(700, 500);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.MouseLeave += new System.EventHandler(this.pictureBox_MouseLeave);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            // 
+            // labelPixelsDesc
+            // 
+            this.labelPixelsDesc.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelPixelsDesc.AutoSize = true;
+            this.labelPixelsDesc.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPixelsDesc.Location = new System.Drawing.Point(15, 14);
+            this.labelPixelsDesc.Name = "labelPixelsDesc";
+            this.labelPixelsDesc.Size = new System.Drawing.Size(79, 21);
+            this.labelPixelsDesc.TabIndex = 1;
+            this.labelPixelsDesc.Text = "Location:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightCoral;
+            this.panel1.Controls.Add(this.btnProcess);
+            this.panel1.Controls.Add(this.btnBrowse);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1107, 59);
+            this.panel1.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Khaki;
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.labelIntensityDesc);
+            this.panel2.Controls.Add(this.panelColor);
+            this.panel2.Controls.Add(this.labelColor);
+            this.panel2.Controls.Add(this.labelPixels);
+            this.panel2.Controls.Add(this.labelColorDesc);
+            this.panel2.Controls.Add(this.labelPixelsDesc);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(798, 59);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(309, 614);
+            this.panel2.TabIndex = 6;
+            // 
+            // panelColor
+            // 
+            this.panelColor.BackColor = System.Drawing.Color.Black;
+            this.panelColor.Location = new System.Drawing.Point(101, 56);
+            this.panelColor.Name = "panelColor";
+            this.panelColor.Size = new System.Drawing.Size(24, 22);
+            this.panelColor.TabIndex = 5;
+            this.panelColor.Visible = false;
+            // 
+            // labelColor
+            // 
+            this.labelColor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelColor.AutoSize = true;
+            this.labelColor.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelColor.Location = new System.Drawing.Point(127, 56);
+            this.labelColor.Name = "labelColor";
+            this.labelColor.Size = new System.Drawing.Size(70, 21);
+            this.labelColor.TabIndex = 4;
+            this.labelColor.Text = "[R, G, B]";
+            this.labelColor.Visible = false;
+            // 
+            // labelPixels
+            // 
+            this.labelPixels.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelPixels.AutoSize = true;
+            this.labelPixels.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPixels.Location = new System.Drawing.Point(100, 14);
+            this.labelPixels.Name = "labelPixels";
+            this.labelPixels.Size = new System.Drawing.Size(85, 21);
+            this.labelPixels.TabIndex = 3;
+            this.labelPixels.Text = "[Location]";
+            this.labelPixels.Visible = false;
+            // 
+            // labelColorDesc
+            // 
+            this.labelColorDesc.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelColorDesc.AutoSize = true;
+            this.labelColorDesc.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelColorDesc.Location = new System.Drawing.Point(15, 57);
+            this.labelColorDesc.Name = "labelColorDesc";
+            this.labelColorDesc.Size = new System.Drawing.Size(55, 21);
+            this.labelColorDesc.TabIndex = 2;
+            this.labelColorDesc.Text = "Color:";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Black;
+            this.panel3.Location = new System.Drawing.Point(101, 96);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(24, 22);
+            this.panel3.TabIndex = 8;
+            this.panel3.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(127, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 21);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "[R, G, B]";
+            this.label2.Visible = false;
+            // 
+            // labelIntensityDesc
+            // 
+            this.labelIntensityDesc.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelIntensityDesc.AutoSize = true;
+            this.labelIntensityDesc.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelIntensityDesc.Location = new System.Drawing.Point(15, 97);
+            this.labelIntensityDesc.Name = "labelIntensityDesc";
+            this.labelIntensityDesc.Size = new System.Drawing.Size(79, 21);
+            this.labelIntensityDesc.TabIndex = 6;
+            this.labelIntensityDesc.Text = "Intensity:";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1107, 673);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelImage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "mainForm";
+            this.Text = "Thermal Camera Image Recognition";
+            this.panelImage.ResumeLayout(false);
+            this.panelImage.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Panel panelImage;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label labelPixelsDesc;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label labelColorDesc;
+        private System.Windows.Forms.Label labelColor;
+        private System.Windows.Forms.Label labelPixels;
+        private System.Windows.Forms.Panel panelColor;
+        private System.Windows.Forms.Button btnConvertToGray;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelIntensityDesc;
+    }
+}
+
---
+++ ThemalCameraImageRecognition/MainForm.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThemalCameraImageRecognition
+{
+    public partial class mainForm : Form
+    {
+        private Bitmap _currentImage;
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            CreateAndOpenFileDialog();
+            CreateAndShowBitmapImage();
+        }
+
+        private void CreateAndShowBitmapImage()
+        {
+            Bitmap originalBitmap = new Bitmap(textBox1.Text);
+            
+            //Resize image but keep aspect ratio
+            float width = 640;
+            float height = 480;
+            float scale = Math.Min(width / originalBitmap.Width, height / originalBitmap.Height);
+            
+            var scaledWidth = (int)(originalBitmap.Width * scale);
+            var scaledHeight = (int)(originalBitmap.Height * scale);
+
+            Bitmap resizedBitmap = new Bitmap(originalBitmap, new Size( scaledWidth, scaledHeight));
+
+            pictureBox.Image = resizedBitmap;
+            _currentImage = resizedBitmap;
+            
+            pictureBox.Location = new Point(panelImage.Width / 2 - pictureBox.Width / 2,
+                panelImage.Height / 2 - pictureBox.Height / 2);
+
+            btnConvertToGray.Enabled = true;
+        }
+
+        private void CreateAndOpenFileDialog()
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog  
+            {  
+                InitialDirectory = @"C:\",  
+                Title = "Browse Image Files",
+                CheckFileExists = true,  
+                CheckPathExists = true,
+                DefaultExt = "jpg",  
+                Filter = "Images (*.BMP;*.JPG;*.GIF,*.PNG,*.TIFF)|*.BMP;*.JPG;*.GIF;*.PNG;*.TIFF",  
+                FilterIndex = 1,  
+                RestoreDirectory = true,
+                ReadOnlyChecked = true,  
+                ShowReadOnly = true  
+            };  
+  
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)  
+            {  
+                textBox1.Text = openFileDialog1.FileName;
+                btnProcess.Enabled = true;
+            }  
+        }
+        
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_currentImage == null)
+                return;
+
+            if (!(Cursor.Current is null)) 
+                Cursor = new Cursor(Cursor.Current.Handle);
+
+            var localMousePosition = pictureBox.PointToClient(Cursor.Position);
+
+            labelPixels.Text = $"[{localMousePosition.X}, {localMousePosition.Y}]";
+
+            if (localMousePosition.X < 0 || localMousePosition.Y < 0)
+                return;
+
+            var pixelColor = _currentImage.GetPixel(localMousePosition.X, localMousePosition.Y);
+            labelColor.Text = $"[{pixelColor.R}, {pixelColor.G}, {pixelColor.B}]";
+            panelColor.BackColor = pixelColor;
+
+            panelColor.Show();
+            labelColor.Show();
+            labelPixels.Show();
+        }
+
+        private void pictureBox_MouseLeave(object sender, EventArgs e)
+        {
+            panelColor.Hide();
+            labelColor.Hide();
+            labelPixels.Hide();
+        }
+
+        private void btnConvertToGray_Click(object sender, EventArgs e)
+        {
+            if(_currentImage == null)
+                return;
+            _currentImage = ConvertBlackAndWhite(_currentImage);
+            pictureBox.Image = _currentImage;
+        }
+
+        private Bitmap ConvertBlackAndWhite(Bitmap Image)
+        {
+            //create a blank bitmap the same size as original
+            Bitmap newBitmap = new Bitmap(_currentImage);
+
+            //get a graphics object from the new image
+            Graphics g = Graphics.FromImage(newBitmap);
+
+            //create the grayscale ColorMatrix
+            ColorMatrix colorMatrix = new ColorMatrix(
+                new float[][]
+                {
+                    new float[] {.3f, .3f, .3f, 0, 0},
+                    new float[] {.59f, .59f, .59f, 0, 0},
+                    new float[] {.11f, .11f, .11f, 0, 0},
+                    new float[] {0, 0, 0, 1, 0},
+                    new float[] {0, 0, 0, 0, 1}
+                });
+            //create some image attributes
+            ImageAttributes attributes = new ImageAttributes();
+            //set the color matrix attribute
+            attributes.SetColorMatrix(colorMatrix);
+            //draw the original image on the new image
+            //using the grayscale color matrix
+            g.DrawImage(_currentImage, new Rectangle(0, 0, _currentImage.Width, _currentImage.Height),
+                0, 0, _currentImage.Width, _currentImage.Height, GraphicsUnit.Pixel, attributes);
+            //dispose the Graphics object
+            g.Dispose();
+
+            return newBitmap;
+        }
+    }
+}
